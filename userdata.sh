f723829097e4202517b091a102c92ca6b98d5cd0
handybrains <33875164+HandyBrains@users.noreply.github.com>
2021-08-13 01:05:57 +0100

    website-url-updated

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
+sudo su
 yum install -y httpd
 service httpd start
 chkonfig httpd on
 echo "<html>
 <h1>You are viwing the Webserver installed using terraform</h1>
 <h2>Please visit us by clicking the below URL</h2>
-<a href="https://www.bsschoolofmusic.co.uk/">https://bsschoolofmusic.co.uk</a>
+<a href="https://www.bbc.co.uk/">https://bbc.co.uk</a>
 </html>" > /var/www/html/index.html
